46f22bc3f6bf60b57323f5148b28f68daebfbec8
maxertivo <102839707+maxertivo@users.noreply.github.com>
2022-04-06 00:22:37 -0500

    Improved graph explorer

--- app/Explorer.hs
+++ app/Explorer.hs
@@ -6,10 +6,19 @@ import Control.Monad
 import Node
 import Graph
 import Data.Maybe
+import Control.Exception
+
+data GraphException = MissingWarp !Id | MissingNode
+                    deriving (Show)
+
+instance Exception GraphException
 
 main :: IO ()
 main = do
-   explore buildNodes [] (getNode buildNodes (R RHiveTotem))
+    case getNode buildNodes (R RHiveTotem) of
+        Just node -> explore buildNodes [] node
+        Nothing -> return ()
+    
   
 
 explore :: [Node] -> [ItemName] -> Node -> IO ()
@@ -18,31 +27,32 @@ explore nodes items node = do
     let predicates = map canUse edgeList
     let ids = map nodeId edgeList
     let bools = eval predicates items
-    print $ roomId node
-    print items
-    print ids
-    print bools
+    
+    putStrLn  "---------------------------------------"
+    putStrLn  $ "Current Room: " ++ show (roomId node)
+    putStrLn  $ "Items: " ++ show items
+    printEdges ids bools
     command <- getLine
-    let index = read command :: Integer
-    let newNodeId = fromMaybe (R OLandingSite) (getIndex ids (index-1))
-    let newNode = getNode nodes newNodeId
-    let allowed = fromMaybe False (getIndex bools (index-1))
+
+    let index = (read command :: Integer) - 1
+    let allowed = fromMaybe False (getIndex bools index)
+    let newNode = getIndex ids index >>= getNode nodes
+
     if allowed 
-        then case newNodeId of
-            I id -> explore nodes (itemName (getNode nodes (I id)) : items) (getNode nodes (R $ warp newNode))
-            R id -> explore nodes items (getNode nodes (R id))
-        else explore nodes items node
+    then case newNode of
+        Just (Item id name warp) -> case getNode nodes (R warp) of 
+                                    Just warpNode -> explore nodes (name : items) warpNode
+                                    Nothing -> throw $ MissingWarp $ R warp
+        Just (Room id edges) -> explore nodes items (Room id edges)
+        Nothing -> throw MissingNode
+    else explore nodes items node
 
 
 
-getNode :: [Node] -> Id ->  Node
-getNode [] x = Room OLandingSite [] -- Hopefully this doesn't happen
-getNode ((Item item name warp):rest) x = case x of 
-                            I id -> if id == item then Item item name warp else getNode rest x
-                            R id -> getNode rest x
-getNode ((Room room edges):rest) x = case x of 
-                            I id -> getNode rest x
-                            R id -> if id == room then Room room edges else getNode rest x
+getNode :: [Node] -> Id ->  Maybe Node
+getNode ((Item item name warp):rest) (I id) = if id == item then return (Item item name warp) else getNode rest (I id)
+getNode ((Room room edges):rest) (R id) = if id == room then return (Room room edges) else getNode rest (R id)
+getNode _ _ = Nothing
 
 getIndex :: [a] -> Integer -> Maybe a
 getIndex [] i = Nothing
@@ -52,10 +62,11 @@ getIndex (x:rest) i = getIndex rest (i-1)
 eval :: [[ItemName] -> Bool] -> [ItemName] -> [Bool]
 eval predicates items = map (\ x -> x items) predicates
 
-printBool :: Bool -> IO ()
-printBool bool = do
-    print bool
+printEdges :: [Id] -> [Bool] -> IO ()
+printEdges  = printEdgesHelper 1
 
-printEdge :: Edge -> IO ()
-printEdge edge = do
-    print $ nodeId edge
+printEdgesHelper :: Integer -> [Id] -> [Bool] -> IO ()
+printEdgesHelper count (id:ids) (bool:bools) = do
+    putStrLn $ show count ++ ". " ++ show id ++ " - " ++ show bool
+    printEdgesHelper (count+1) ids bools
+printEdgesHelper _ _ _ = return ()
